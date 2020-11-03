00eb9dbd25e4153bffd0e2909cd49313f87c0a81
Valentín Nicheglod <nicheglod69@gmail.com>
2020-11-03 16:22:43 -0300

    ModeloProducto

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -6,7 +6,41 @@ module.exports = (sequelize) => {
   sequelize.define('product', {
     name: {
       type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    pictures: {
+      type: DataTypes.ARRAY(DataTypes.TEXT),
       allowNull: false,
     },
+    brand: {
+      type: DataTypes.STRING
+    },
+    model: {
+      type: DataTypes.STRING
+    },
+    asessment: {
+      type: DataTypes.STRING
+    },
+    firstCategory: {
+      type: DataTypes.ENUM('a definir', 'another value')
+    },
+    secondCategory: {
+      type: DataTypes.ENUM('a definir', 'another value')
+    }
   });
 };
+
+// Otra posible forma para categorias...
+// type: DataTypes.ARRAY(DataTypes.ENUM('a definir', 'another value')) 
