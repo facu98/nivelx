99083dbed8baf84de892eda4ec906baee06b4b6d
lzambranoa <lzambranoa.software@gmail.com>
2020-11-05 09:10:38 -0500

    create(api/routes/product.js):Creacion ruta productos keyword

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -9,4 +9,25 @@ server.get('/', (req, res, next) => {
 		.catch(next);
 });
 
+/* Retorna los productos que tengan query={valor} en su nombre
+o descripción (/search?query={valor})*/
+
+server.get('/search', (req, res) => {
+	
+	const { name, description } = req.query;
+
+	Product.findAll( {
+		where: {
+			[Op.or]: [
+				{name: {[Op.substring]: `${name}`}},
+				{description: {[Op.substring]: `${description}`}}
+			],
+		},
+	})
+	.then((products) => !products ? res.status(400).send('No hay productos asociados con la busqueda')
+								: res.send(products))
+
+	.catch((err) => res.status(404).send(err));
+});
+
 module.exports = server;
