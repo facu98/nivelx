4ba348c9cc163f0147f39c03572fb3e8600c286f
facu <facuortiz.rg@gmail.com>
2020-11-04 23:49:16 -0300

    S26 (api/src/routes/product.js) PUT para actualizar productos

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -9,4 +9,58 @@ server.get('/', (req, res, next) => {
 		.catch(next);
 });
 
+server.post("/", (req,res) => {
+	const {name, description, price, stock, pictures, brand, model , asessment, firstCategory, secondCategory} = req.body
+	if(!name || !description || !price || !stock || !pictures){return res.status(400).send("Debe rellenar los campos requeridos")}
+	Product.findOne({
+		where: {
+			name, description, price, stock, pictures, brand, model
+		}
+	})
+	.then((product) => {
+		if(product) {return res.status(400).send("Ya existe el producto")}
+
+		const newProduct = Product.create({
+			name,
+			description,
+			price,
+			stock,
+			pictures,
+			brand,
+			model,
+			asessment,
+			firstCategory,
+			secondCategory
+		})
+		.then((product) => res.status(201).send(product))
+	})
+	.catch((err) => res.status(400).send(err))
+})
+
+server.put("/:id", (req,res) => {
+	const {id} = req.params
+	const {name, description, price, stock, pictures, brand, model , asessment, firstCategory, secondCategory} = req.body
+	if(Object.entries(req.body).length < 1 ){return res.status(400).send("Debe rellenar algun campo")}
+
+	Product.findByPk(id)
+	.then((product) => {
+		if(!product){return res.status(400).send("ID inexistente")}
+
+		name && (product.name = name)
+		description && (product.description = description)
+		price && (product.price = price)
+		stock && (product.stock = stock)
+		pictures && (product.pictures = pictures)
+		brand && (product.brand = brand)
+		model && (product.model = model)
+		asessment && (product.asessment = asessment)
+		firstCategory && (product.firstCategory = firstCategory)
+		secondCategory && (product.secondCategory = secondCategory)
+		product.save()
+		.then((producto) => res.send(product))
+		.catch((err) => res.send(err))
+		})
+	.catch(() => {return res.status(400).send("ID invalido")})
+})
+
 module.exports = server;
