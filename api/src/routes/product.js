f26768ae46e9304291587aee41fb678961ddf6ae
Lucas G <g.brianlucass1993@gmail.com>
2020-11-04 23:28:44 -0300

    S24 (\api\src\routes\product.js) creacion de ruta product x id

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,5 +1,6 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
+const { Op } = require('sequelize');
 
 server.get('/', (req, res, next) => {
 	Product.findAll()
@@ -7,6 +8,19 @@ server.get('/', (req, res, next) => {
 			res.send(products);
 		})
 		.catch(next);
+});
+
+// Busco el producto por id y muestro sus datos(incluida categoria e imagenes)
+server.get('/:id', (req, res, next) => {
+	const id = req.params.id;
+
+	const productById = Product.findByPk(id);
+	if (!productById) {
+		return res.status(400).send('The product does not exist');
+	} else {
+		res.status(200).send(productById);
+	}
+
 });
 
 module.exports = server;
