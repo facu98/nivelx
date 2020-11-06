ff13387010d5e4c432856cdf8cf816304392bff5
Felipe Blanco Muzzolon <felipeblanco114@gmail.com>
2020-11-06 15:48:32 -0300

    Arreglo en el Form de Productos

---
+++ client/src/components/FormProduct/FormProduct.jsx
@@ -0,0 +1,113 @@
+import React, {useState} from 'react';
+import style from './Product.module.css';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const data = [
+    { id: 1, name: "Lavarropas", description: "0 km", price: 50000, stock: 6, pictures: "", brand: "Drean", model: "R200", valoration: "9", category: "Electro", category2: "Home" },
+    { id: 2, name: "Secarropas", description: "0 km", price: 20000, stock: 2, picture: "", brand: "Drean", model: "R200", valoration: "9", category: "Electro", category2: "Home" },
+    { id: 3, name: "PlayStation 3", description: "0 km", price: 40000, stock: 4, picture: "", brand: "Sony", model: "Slim", valoration: "9", category: "Game", category2: "Enterteinment" }
+]
+
+export default function ProductCRUD({ match }){
+    const [input, setInput] = useState({
+        name: "",
+        brand: "",
+        price: "",
+        pictures: "",
+        category: [],
+        stock: "",
+        description: "",
+    })
+
+
+    const handleInputChange = (e)=>{
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    };
+    const resetForm = ()=> {
+        setInput({
+            name: "",
+            brand: "",
+            price: "",
+            pictures: "",
+            category: [],
+            stock: "",
+            description: "",
+        })
+    };
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const newProduct = { 
+            name: input.name, 
+            brand: input.brand,
+            price: input.price,
+            pictures: input.pictures,
+            category: input.category,
+            stock: input.stock,
+            description: input.description,
+        }
+        fetch('http://localhost:3001/products', {
+            method: 'POST',
+            body: JSON.stringify(newProduct),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              }
+        })
+        .then(()=>{
+            alert(`Se ha creado un nuevo producto exitosamente`)
+            resetForm();
+        })
+        .catch((err)=>{
+             console.log(err)
+        })
+    }
+    return (
+        <div className={style.formStyle}>
+            <h3>Crear Producto</h3>
+            <hr/>
+            <form onSubmit={handleSubmit} >
+                <div className={style.inputContainer}>
+                    <label>Nombre del producto</label>
+                    <input type="text" name="name" onChange={handleInputChange} value={input.name} required autoFocus />
+                </div>
+                <div className={style.inputContainer}>
+                    <label>Marca del producto</label>
+                    <input type='text' name='brand' onChange={handleInputChange} value={input.brand} required autoFocus />
+                </div>
+                <div className={style.inputContainer}>
+                    <label>Precio del producto</label>
+                    <input type="number" name="price" onChange={handleInputChange} value={input.price} required autoFocus  />
+                </div>
+                <div className={style.inputContainer}>
+                    <label>Imagen del producto</label>
+                    <input type="file" name="pictures" onChange={handleInputChange} value={input.pictures} required autoFocus />
+                </div>
+                <div className={style.inputContainer}>
+                    <label>Categoría</label>
+                    <input type='text' name='category' onChange={handleInputChange} value={input.category} required autoFocus />
+                </div>
+                <div>
+                    <label className={style.labelStock}>Stock</label>
+                    <input className={style.inputStock} type='number' name='stock' onChange={handleInputChange} value={input.stock} required autoFocus />
+                </div>
+                <div className={style.inputContainer}>
+                    <label>Descripción del producto</label>
+                    <textarea name="description" onChange={handleInputChange} value={input.description} required />
+                </div>
+
+                <div className="buttonContainer">
+                    <button onClick={resetForm} className="button"> Cancelar </button>
+                    <input  type="submit" value="Guardar" className="button"/>
+                </div>
+
+            </form>
+        </div>
+    )
+}
+
---
+++ d
@@ -0,0 +1,26 @@
+  ProductCrudFixs[m
+* [32mmaster[m
+  [31mremotes/origin/DOCUMENTATION[m
+  [31mremotes/origin/Fix-Components[m
+  [31mremotes/origin/HEAD[m -> origin/master
+  [31mremotes/origin/ProductCrudFixs[m
+  [31mremotes/origin/S08-FormularioCRUD-Prod[m
+  [31mremotes/origin/S11-React-Router[m
+  [31mremotes/origin/S13-Crear-Componente-Catalogo[m
+  [31mremotes/origin/S22-Crear-Ruta-que-devuelva-los-productos-de-X-categoria[m
+  [31mremotes/origin/S23-Crear-Ruta-retorna-productos-keyword[m
+  [31mremotes/origin/S24-Crear-ruta-de-producto-individual,-pasado-un-ID-que-retorne-un-producto-con-sus-detalles[m
+  [31mremotes/origin/S30-CrearModeloUser[m
+  [31mremotes/origin/S4-Crear-Modelo-de-Productos[m
+  [31mremotes/origin/S5-Model-Category[m
+  [31mremotes/origin/S6-Crear-Componente-Producto[m
+  [31mremotes/origin/SearchBar[m
+  [31mremotes/origin/fix-s10-s9-s14[m
+  [31mremotes/origin/fix-structure[m
+  [31mremotes/origin/master[m
+  [31mremotes/origin/product-card[m
+  [31mremotes/origin/s10-s9-s14[m
+  [31mremotes/origin/s9-s14[m
+  [31mremotes/origin/scicolonejulian-patch-1[m
+  [31mremotes/origin/scicolonejulian-patch-2[m
+  [31mremotes/origin/scicolonejulian-patch-3[m
