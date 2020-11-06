9875cf4b44e47723d675673d2e65ea4bf6e07f36
Lucas G <g.brianlucass1993@gmail.com>
2020-11-06 09:59:00 -0300

    update (\client\src\components\ProductDetail) modifique product.js y su css

--- client/src/components/ProductDetail/Product.js
+++ client/src/components/ProductDetail/Product.js
@@ -5,26 +5,66 @@ const productTemplate = ({ title, description, price, quantity }) => {
 
 }
 
+// funcionalidad para las imagenes pequeñas
+/*
+let productImg = document.getElementById("ProductImg");
+let smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function () {
+    productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function () {
+    productImg.src = smallImg[0].src;
+}
+smallImg[2].onclick = function () {
+    productImg.src = smallImg[0].src;
+}
+smallImg[3].onclick = function () {
+    productImg.src = smallImg[0].src;
+}
+*/
+
 export default function Product(selectedProduct) {
     return (
-        <div className={styles.cardMargin}>
-            <table>
-                <tr>
-                    <th>{selectedProduct.title}</th>
-                    <th>{selectedProduct.price}</th>
-                </tr>
-                <tr>
-                    <th>{selectedProduct.description}</th>
-                </tr>
-                <tr>
-                    <th>cantidad</th>
-                </tr>
-                <tr>
-                    <th>{selectedProduct.quantity}</th>
+        <div>
+            <div class = "small-container single-product">
+                <div class = "row">
+                    <div class = "col-2">
+                        {/*src= "images/gallery-1.jpg" width="100%" id= "product-img"*/}
+                        <img>Im a img</img>
+                        <div class= "small-img-row">
+                            <div class= "small-img-col">
+                                {/*src= "images/gallery-1.jpg" width="100%" class="small-img"*/}
+                                <img>img 1</img>
+                            </div>
+                            <div class= "small-img-col">
+                                <img>img 2</img>
+                            </div>
+                            <div class= "small-img-col">
+                                <img>img 3</img>
+                            </div>
+                            <div class= "small-img-col">
+                                <img>img 4</img>
+                            </div>
+                        </div>
+                    </div>
+                    <div class = "col-2">
+                        <p>{selectedProduct.category}</p>
+                        <h1>{selectedProduct.title}</h1>
+                        <h4>{selectedProduct.price}</h4>
+                        {/* 
+                        <select>
+                            <option>Select options</option>
+                        </select>
+                        */}
+                        <input type="number" value= "1"> quantity to buy</input>
 
-                </tr>
-            </table>
-
+                        <h3>Details<i class="fa fa-indent"></i></h3>
+                        <br/>
+                        <p>{selectedProduct.description}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
