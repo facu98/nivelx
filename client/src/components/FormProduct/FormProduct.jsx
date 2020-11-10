471ba77df50bacff446d6d687bab0d12142b8b16
facu <facuortiz.rg@gmail.com>
2020-11-10 17:48:01 -0300

    Fix (client/components)  ahora se puede seleccionar solo las categorias existentes

--- client/src/components/FormProduct/FormProduct.jsx
+++ client/src/components/FormProduct/FormProduct.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import style from './Product.module.css';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
@@ -6,11 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const data = [
     { id: 1, name: "Lavarropas", description: "0 km", price: 50000, stock: 6, pictures: "", brand: "Drean", model: "R200", valoration: "9", category: "Electro", category2: "Home" },
-    { id: 2, name: "Secarropas", description: "0 km", price: 20000, stock: 2, pictures: "", brand: "Drean", model: "R200", valoration: "9", category: "Electro", category2: "Home" },
-    { id: 3, name: "PlayStation 3", description: "0 km", price: 40000, stock: 4, pictures: "", brand: "Sony", model: "Slim", valoration: "9", category: "Game", category2: "Enterteinment" }
+    { id: 2, name: "Secarropas", description: "0 km", price: 20000, stock: 2, picture: "", brand: "Drean", model: "R200", valoration: "9", category: "Electro", category2: "Home" },
+    { id: 3, name: "PlayStation 3", description: "0 km", price: 40000, stock: 4, picture: "", brand: "Sony", model: "Slim", valoration: "9", category: "Game", category2: "Enterteinment" }
 ]
 
 export default function ProductCRUD({ match }){
+  const [categorias, setCategorias] = useState([])
+
     const [input, setInput] = useState({
         name: "",
         brand: "",
@@ -19,16 +21,36 @@ export default function ProductCRUD({ match }){
         category: [],
         stock: "",
         description: "",
-        color: [],
     })
+
+    useEffect(() => {
+      fetch(`http://localhost:3001/category`)
+        .then(function (response) {
+          return response.json()
+        })
+        .then(function (category) {
+          setCategorias(category)
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+    }, [])
 
 
     const handleInputChange = (e)=>{
         setInput({
             ...input,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
         })
     };
+
+    const categoryChange = (e) => {
+    const id = parseInt(e.target.value)
+    const finder = input.category.find((cat) => cat === id)
+    finder ? input.category = input.category.filter((cat) => cat !== id) : input.category.push(id)
+
+    }
+
     const resetForm = ()=> {
         setInput({
             name: "",
@@ -38,25 +60,23 @@ export default function ProductCRUD({ match }){
             category: [],
             stock: "",
             description: "",
-            color: [],
         })
     };
 
     const handleSubmit = (e)=>{
-      var img = input.pictures.replace("C:\\fakepath\\", "http://localhost:3000/assets/img/")
         e.preventDefault();
         const newProduct = {
             name: input.name,
             brand: input.brand,
             price: input.price,
-            pictures: img.split(),
-            category: input.category.replace(" ", "").split(","),
+            pictures: ["input.pictures"],
+            category: input.category,
             stock: true,
             description: input.description,
-            quantity: input.stock,
-            color: input.color.split(),
-        }
+            quantity: input.stock
 
+        }
+        console.log(JSON.stringify(newProduct))
         fetch('http://localhost:3001/products', {
             method: 'POST',
             body: JSON.stringify(newProduct),
@@ -73,6 +93,7 @@ export default function ProductCRUD({ match }){
              console.log(err)
         })
     }
+
     return (
         <div className={style.formStyle}>
             <h3>Crear Producto</h3>
@@ -96,18 +117,17 @@ export default function ProductCRUD({ match }){
                 </div>
                 <div className={style.inputContainer}>
                     <label>Categoría</label>
-                    <input type='text' name='category' onChange={handleInputChange} value={input.category} required autoFocus />
+                    {categorias && categorias.map((cat) => {
+                      return (<div>
+                        <input type="checkbox" name={cat.name} id={cat.id} value={cat.id} onChange={categoryChange}/>
+                        <label for={cat.id}>{cat.name}</label>
+                        </div>)
+                    })}
                 </div>
                 <div>
                     <label className={style.labelStock}>Stock</label>
                     <input className={style.inputStock} type='number' name='stock' onChange={handleInputChange} value={input.stock} required autoFocus />
                 </div>
-
-                <div>
-                    <label className={style.labelStock}>Color</label>
-                    <input className={style.inputStock} type='text' name='color' onChange={handleInputChange} value={input.color} required autoFocus />
-                </div>
-
                 <div className={style.inputContainer}>
                     <label>Descripción del producto</label>
                     <textarea name="description" onChange={handleInputChange} value={input.description} required />
