ffdb258346ea05b8d36f9dfcdd463a87a0ae3b7f
Valentín Nicheglod <nicheglod69@gmail.com>
2020-11-11 17:40:15 -0300

    CREATE (api/src/routes/order.js) Creo ruta para mostrar una orden en particular

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -1,8 +1,15 @@
 const server = require('express').Router();
 const { Order } = require('../db.js');
 
-server.get("/orders", (req, res) => {
+server.get("/", (req, res) => {
 	Order.findAll()
 	.then(orders => res.send(orders))
+	.catch(err => res.status(404).send(err))
+});
+
+server.get("/:id", (req, res) => {
+	const id = req.params.id;
+	Order.findByPk(id)
+	.then(order => res.send(order))
 	.catch(err => res.status(404).send(err))
 });
