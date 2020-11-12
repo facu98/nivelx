91fb86e76c6596a971ab11603667ca2dd83670d4
facu <facuortiz.rg@gmail.com>
2020-11-12 15:34:34 -0300

    create form user

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,5 +1,5 @@
 const server = require('express').Router();
-const { User } = require('../db.js');
+const { User, Order } = require('../db.js');
 const { Op } = require('sequelize')
 const trash = [];
 
@@ -63,4 +63,135 @@ server.post("/", (req,res) => {
 	.catch((err) =>  res.send(err))
 });
 
+
+//
+// ELIMINA EL usuario
+server.delete('/:id', (req, res) => {
+	User.findByPk(req.params.id)
+		.then((user) => {
+			user.destroy().then((user) => {
+				res.status(200).send(user)
+			})
+		})
+		.catch(() => res.status(404).send('Id no valido'))
+})
+
+
+
+// Eliminar carrito
+
+server.delete('/:idUser/cart/', (req, res) => {
+    let id = req.params.idUser;
+	Order.findOne({
+		where: {
+			user_id: id
+		}
+	})
+	.then( cart => {
+		trash.push(cart);
+		cart.destroy()
+		res.send('Carrito vaciado');
+	})
+	.catch(err => {
+		res.status(500).send(err);
+	});
+});
+
+server.delete('/:id', (req, res) => {
+	User.findByPk(req.params.id)
+		.then((user) => {
+			user.destroy().then((user) => {
+				res.status(200).send(user)
+			})
+		})
+		.catch(() => res.status(404).send('Id no valido'))
+})
+
+// Editar cantidad del carrito
+
+server.put('/:idUser/cart', (req, res) => {
+	let id = req.params.idUser;
+	const { quantity } = req.body;
+	if(Object.entries(req.body).length < 1 ){return res.status(400).send("Debe rellenar este campo")}
+
+	Order.findOne({
+		where: {
+			user_id: id
+		}
+	})
+	.then(cart => {
+		if(!cart) {
+			return res.status(400).send("No se encuentra el carrito requerido")
+		}
+		quantity && (cart.quantity = quantity)
+		cart.save()
+		.then((cart) => res.send(cart))
+		.catch((err) => res.send(err))
+	})
+	.catch((err) => {
+		console.log(err);
+		return res.status(400).send("No se encuentra la información requerida");
+	})
+})
+
+//Retorna todos los items del Carrito
+
+server.get(('/:idUser/cart'), (req, res, next) => {
+    const id = req.params.idUser;
+
+    User.findByPk(id, {
+        where: {
+            idOrder: id
+        },
+        include: {
+            model: Order
+        }
+    })
+        .then(contentOrder => {
+            if (!contentOrder) {
+                return res.status(400).send('Order does not exist');
+            }
+            res.send(contentOrder);
+        })
+        .catch(next);
+});
+
+// Retorna las órdenes del usuario
+
+server.get('/:id/orders', (req, res) => {
+	let id = req.params.id;
+	Order.findAll({
+	where: {
+		user_id: id
+		}
+	})
+	.then(orders => res.send(orders))
+	.catch(err => res.status(404).send(err));
+});
+
+// Modifica las órdenes del usuario
+
+server.put('/:id/orders', (req, res) => {
+	const { state, date } = req.body
+	let id = req.params.id;
+	Order.findAll({
+	where: {
+		user_id: id
+		}
+	})
+	.then(order => {
+		if(!order){
+			res.status(400).send(`No se encuentran órdenes de este usuario`);
+		}
+		if( !state || !date ){
+			res.status(400).send(`Debe completar los campos obligatorios`);
+		}
+
+		order.state = state;
+		order.date = date;
+		order.save()
+		.then(order => res.send(order))
+		.catch(err => res.status(404).send(err))
+	});
+})
 module.exports = server;
--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -4,6 +4,7 @@ import './SearchBar.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import SearchIcon from '@material-ui/icons/Search';
 import { NavLink, useHistory } from 'react-router-dom'
+import { Navbar } from '../NavBar/NavBar';
 
 
 
@@ -22,47 +23,28 @@ const SearchBar = () => {
     });
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className={"container"}>
-                <NavLink to='/' className="navbar-brand">
-                    <h1>Nivel X</h1>
-                </NavLink>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <NavLink to="/" className='nav-link' >
-                                Productos
-                        </NavLink>
-                        </li>
-                        <li className="nav-item active">
-                            <NavLink to="/products/" className='nav-link' >
-                                Juegos
-                        </NavLink>
-                        </li>
-                        <li className="nav-item active">
-                            <NavLink to="/products/" className='nav-link' >
-                                Login
-                        </NavLink>
-                        </li>
-                        <li className="nav-item active">
-                            <NavLink to="/admin/panel" className='nav-link' >
-                                Admin
-                            </NavLink>
-                        </li>
-
-
-                    </ul>
-                    <form onSubmit={formik.handleSubmit} className="form-inline my-2 my-lg-0">
+        <div >
+            <div className="container">
+                <div  className="row justify-content-center justify-content-md-start m-3">
+                <div className="col-md-4 text-center" >
+                    <NavLink to="/" className='nav-link'>
+                    {/* <img src="https://www.canva.com/design/DAEMshrihhw/Y4Sh9WKSEbRiBDliw88EVA/view?utm_content=DAEMshrihhw&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" alt="Logo_store" /> */}
+                    <h1>NIVEL X</h1>
+                    </NavLink>
+                </div>
+                <div className="col-md-8 m-auto" >
+                    <form onSubmit={formik.handleSubmit} className="form-inline my-2 my-lg-0 justify-content-center">
                         <input  onChange={formik.handleChange} className="form-control mr-sm-2 input-large" type="text" name="query" placeholder="Busca tu producto!" aria-label="Search" />
                         <button  className="btn btn-outline-success my-2 my-sm-0" type="submit"><SearchIcon /></button>
                     </form>
                 </div>
+                </div>
             </div>
-        </nav>
+            <Navbar />
+        </div>
+            
+       
 
     )
 }
