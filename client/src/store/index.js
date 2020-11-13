ff3ce77f76c38050b295dace4d46ccc4aaa2739f
Lucas G <g.brianlucas1993@gmail.com>
2020-11-13 15:48:39 -0300

    Update S42

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import rootReducer from "../reducers/index";
 import thunk from "redux-thunk";
+import { orderReducer } from "../reducers/orderReducers";
 
 
 
 const store = createStore(rootReducer,
-
+  combineReducers({
+    order: orderReducer
+  }),
   compose(applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
