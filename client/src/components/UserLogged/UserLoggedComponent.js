7157940b0cc521c8f06a5dfa53cab450975ea155
Felipe Blanco Muzzolon <felipeblanco114@gmail.com>
2020-11-20 16:45:10 -0300

    userLogged

---
+++ client/src/components/UserLogged/UserLoggedComponent.js
@@ -0,0 +1,52 @@
+import React, {useEffect} from 'react';
+import { NavLink, useHistory } from 'react-router-dom'
+// IMPORTS DE MATERIAL UI
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import {useDispatch, useSelector} from "react-redux"
+import {getProductsCart, logOut} from "../../actions"
+
+
+export default function UserLogged() {
+
+    const user = useSelector(state => state.user);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+  
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    const logOutAndClose = () => {
+        handleClose();
+        if(window.confirm(`Seguro que deseas cerrar sesión?`)){
+            dispatch(logOut())
+        };
+    };
+  
+    return (
+      <div>
+        <Button color='secondary' aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+          { user.id ? user.name : 'Registrarse' }
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>Mis compras</MenuItem>
+          {/* <MenuItem onClick={handleClose}></MenuItem> */}
+          <MenuItem onClick={logOutAndClose}>Salir</MenuItem>
+        </Menu>
+      </div>
+    );
+  }
