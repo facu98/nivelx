39c0bc8d89ccb8cba2d1dcd9f61725110516f23f
Sharkman <scicolone.julian@gmail.com>
2020-11-26 02:20:16 -0300

    Copyright1.1

--- client/src/components/Copyright/Copyright.js
+++ client/src/components/Copyright/Copyright.js
@@ -7,7 +7,7 @@ export default function () {
     return (
         <div className='Copyright-Container'>
           <div className='Copyright-text'>
-            <span>Hecho con 💛 por alumnos de Henry.</span>
+            <span>Hecho con 💛 por alumnos de Henry FT06-G10.</span>
             <span> Henry © {new Date().getFullYear() }  | Todos los derechos reservados. </span>
           </div>
         </div>
